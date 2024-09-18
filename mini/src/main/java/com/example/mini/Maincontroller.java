85b6684eefc6723a6b79f11c896cd2819990d3d4
cyrilsibichan <cyril.sibichan05@gmail.com>
2024-09-18 18:59:29 +0530

    Add files via upload

---
+++ mini/src/main/java/com/example/mini/Maincontroller.java
@@ -0,0 +1,41 @@
+package com.example.mini;
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Maincontroller {
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    public void switchToTaskManager(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("CreateAccount.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void switchToAttendance(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void switchToDashBoard(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("dashboard.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+}
+
